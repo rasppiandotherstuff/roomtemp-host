8753849127581504ded80a2a30315e216ddbce3c
rasppiandotherstuff <mrbmlmania@gmail.com>
2025-04-28 16:47:16 -0700

    Save fixed upload_temp.sh

--- upload_temp.sh
+++ upload_temp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 cd /home/jajko/roomtemp-host
+
+export GIT_TERMINAL_PROMPT=0
+export HOME=/home/jajko
+
 cp /home/jajko/roomtemp.txt ./roomtemp.txt
+
 git add roomtemp.txt
-git commit -m "Update temp $(date)"
+git commit -m "Update temp $(date '+%Y-%m-%d %H:%M:%S')"
 git push
