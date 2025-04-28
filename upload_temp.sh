ad3a2daf01eb72e451a5519ee294a3eae8e297c3
rasppiandotherstuff <mrblmania@gmail.com>
2025-04-28 16:09:54 -0700

    Update upload_temp.sh

--- upload_temp.sh
+++ upload_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/pi/roomtemp-host   # change this to where you cloned
-cp /home/pi/roomtemp.txt ./roomtemp.txt
+cd /home/jajko/roomtemp-host
+cp /home/jajko/roomtemp.txt ./roomtemp.txt
 git add roomtemp.txt
 git commit -m "Update temp $(date)"
 git push
